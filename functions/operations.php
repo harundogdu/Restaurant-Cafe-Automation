6d2a8f6867b53a81e23fd2831ed273777e12ecea
Harun Doğdu <harundogdu06@gmail.com>
2021-11-05 00:45:41 +0300

    ürün listelenmesi değiştirildi

--- functions/operations.php
+++ functions/operations.php
@@ -11,9 +11,42 @@ if (isset($_GET)) {
                 $tableId = htmlspecialchars($_GET['id']);
                 $orders = $system->mainQuery($database, "SELECT * FROM orders INNER JOIN products ON orders.productId = products.id where tableId='{$tableId}'");
                 if ($orders) {
+                    $totalAmount = 0;
+                    $totalPrice = 0;
+                    echo "
+                            <table class='table text-center table-striped'>
+                                <thead class='table-dark'>
+                                    <tr>
+                                        <th>Ürün</th>
+                                        <th>Adet</th>
+                                        <th>Fiyat</th>
+                                        <th>İşlem</th>
+                                    </tr>
+                                </thead>
+                                <tbody class='text-center'>
+                        ";
                     foreach ($orders as $order) {
-                        echo "<div class='alert alert-info text-center m-2'>" . $order["name"] . " x " . $order['amount'] . "</div>";
+                        $totalAmount += $order['amount'];
+                        $totalPrice += $order['price'] * $order['amount'];
+                        echo "
+                                    <tr>
+                                        <td>" . $order["name"] . "</td>
+                                        <td>" . $order["amount"] . "</td>
+                                        <td>" . $order["price"] . "₺</td>
+                                        <td><a href='' class='text-danger font-weight-bolder'>Sil</a></td>
+                                    </tr>
+                                ";
                     }
+                    echo "
+                                    <tr class='bg-dark text-white'>
+                                        <td>Toplam</td>
+                                        <td class='border-right border-left border-light'>" . $totalAmount . "</td>
+                                        <td colspan='2' class='text-warning font-weight-bold'>" . $totalPrice . "₺</td>
+                                    </tr>
+                                </tbody>
+                            </table>          
+                            <button class='btn bgColorOrange w-100 font-weight-bold mb-3'>Hesabı Al</button>            
+                       ";
                 } else {
                     echo "<div class='alert alert-warning text-center mx-3'>Henüz sipariş yok!</div>";
                 }
@@ -25,7 +58,7 @@ if (isset($_GET)) {
                     foreach ($products as $product) {
                         echo " 
                             <label class='btn btn-dark m-1'>
-                                <input type='radio' name='productId' value='" . $product['id'] . "' />". $product['name']." 
+                                <input type='radio' name='productId' value='" . $product['id'] . "' />" . $product['name'] . " 
                             </label>";
                     }
                 } else {
@@ -38,13 +71,18 @@ if (isset($_GET)) {
                     $productId = htmlspecialchars($_POST['productId']);
                     $amount = htmlspecialchars($_POST['amount']);
 
-                    $addOperation = $system->mainQuery($database, "insert into orders (tableId,productId,amount) VALUES ($tableId,$productId,$amount)");
+                    $order = $system->mainQuery($database, "select * from orders INNER JOIN products ON orders.productId = products.id where orders.productId=$productId AND orders.tableId=$tableId", true);
+                    if ($order) {
+                        $amount += $order["amount"];
+                        $addOperation = $system->mainQuery($database, "update orders SET amount=$amount where productId=$productId");
+                    } else {
+                        $addOperation = $system->mainQuery($database, "insert into orders (tableId,productId,amount) VALUES ($tableId,$productId,$amount)");
+                    }
                     if ($addOperation) {
                         echo "200";
                     } else {
                         echo "400";
                     }
-
                 } else {
                     echo "999";
                     return;
