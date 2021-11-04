c1869e6e8d894ce9b65e079d9e444131a8a2b037
Harun Doğdu <harundogdu06@gmail.com>
2021-11-05 00:45:21 +0300

    bazı tasarımsal değişiklikler

--- pages/details.php
+++ pages/details.php
@@ -8,23 +8,25 @@ if ($_GET && $_GET['id'] !== null) {
     $table = $system->mainQuery($database, "SELECT * FROM tables where id= '{$tableId}'", true);
     if ($table) {
         echo "
-    <div class='container-fluid'>
+    <div class='container-fluid h-100' >
     <div class='row'>
             <a href='" . SITE_URL . "' class='btn btn-dark btn-block py-2 rounded-0'>Geri Dön</a> 
         </div>
-         <div class='row w-100' style='min-height: 600px'>
-            <div class='col-md-2 p-0 m-0 detail-first-div'>
+         <div class='row h-75'>
+            <div class='col-md-3 p-0 m-0 detail-first-div'>
                 <div class='text-white w-100 p-5 text-center detail-table-name-container'>
                     <p class='table-details-name'>" . $table["name"] . "</p>
                 </div>
-                <div id='dataFromShow'></div>
+                <div id='dataFromShow' class='px-3'></div>
             </div>
-            <div class='col-md-8 detail-second-div'>
+            <div class='col-md-7 detail-second-div'>
                 <form id='addForm'>
-                    <div class='row' style='min-height: 600px'>
+                    <div class='row' style='height: 90vh;'>
                         <div class='col-md-12'>
                             <div class='row h-75'>
-                                <div class='col-md-12' id='productList'></div> 
+                                <div class='col-md-12' id='productList'>
+                                    <img src='./assets/images/kategoriSec.png' alt='Kategori Seçiniz'>
+                                </div> 
                             </div> 
                             <div class='row h-25 d-flex justify-content-between align-items-center'>
                                  <div class='col-md-6 '>
@@ -35,7 +37,7 @@ if ($_GET && $_GET['id'] !== null) {
         for ($i = 1; $i < 16; $i++) {
             echo "<label class='btn bgColorPurple m-1'>
                                                 <input type='radio' name='amount' value='" . $i . "' /> " . $i . "
-                                                </label>";
+                                              </label>";
         }
         echo "   
                                  </div>
