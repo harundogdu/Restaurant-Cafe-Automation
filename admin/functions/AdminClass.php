afb759f5b0c374c7f837b02c31adb04a9b0c6ff7
Harun Doğdu <harundogdu06@gmail.com>
2021-11-06 20:42:32 +0300

    masa crud işlemleri yapıldı

--- admin/functions/AdminClass.php
+++ admin/functions/AdminClass.php
@@ -75,6 +75,105 @@ class AdminClass
         setcookie('username', null, -1, '/Restaurant-Cafe-Automation/admin');
         $this->redirectToLink(self::PANEL_URL);
     }
-}
 
-?>
+    public function tables($database)
+    {
+        echo "
+            <table class='table table-striped text-center'>                
+                <div class='text-right'>
+                    <a href='dashboard.php?page=add-tables' class='btn btn-success mb-2'>Masa Ekle</a>
+                </div>
+                <thead class='table-dark'>
+                    <tr>
+                        <th>Masalar</th>
+                        <th>İşlemler</th>
+                    </tr>  
+                </thead>
+                <tbody>";
+        $tables = $this->mainQuery($database, 'select * from tables');
+
+        foreach ($tables as $table) {
+            echo "
+            <tr>
+                <td class='border-right'>" . $table['name'] . "</td>
+                <td>
+                    <a href='dashboard.php?page=update-tables&id=" . $table['id'] . "' class='btn btn-warning'>Güncelle</a>
+                    <a href='dashboard.php?page=delete-tables&id=" . $table['id'] . "' class='btn btn-danger'>Sil</a>
+                </td>
+            </tr>
+            ";
+        }
+
+        echo "</tbody>
+            </table>
+            ";
+    }
+
+    public function addTables($database){
+        if (isset($_POST['tableName'])) {
+            $tableName = $_POST['tableName'];
+            if ($this->mainQuery($database, "insert into tables (name) VALUES ('$tableName')")) {
+                $this->redirectToLink(self::DASHBOARD_URL);
+            }
+        } else {                       
+                echo "
+                <h5 class='text-center'>Masa Ekle</h5>
+                    <div class='row'>
+                        <div class='col-md-4 mx-auto mt-5 p-3 border'>
+                            <form action='' method='post'>
+                                <div class='form-row my-2'>
+                                    <input type='text' name='tableName' placeholder='Masa adı giriniz.' class='form-control'/>
+                                </div>
+                                <div class='form-row d-flex align-items-center justify-content-end'>
+                                    <input type='submit' name='btnUpdate' value='Ekle' class='btn btn-info' />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                ";         
+        }
+    }
+
+    public function updateTables($database)
+    {
+        if (isset($_POST['tableId'])) {
+            $tableId = $_POST['tableId'];
+            $tableName = $_POST['tableName'];
+            if ($this->mainQuery($database, "update tables SET name='$tableName' where id=$tableId")) {
+                $this->redirectToLink(self::DASHBOARD_URL);
+            }
+        } else {
+            if (isset($_GET['id'])) {
+                $tableId = $_GET['id'];
+                $table = $this->mainQuery($database, 'select * from tables where id=' . $tableId, true);
+                echo "
+                <h5 class='text-center'>Masa Güncelle</h5>
+                    <div class='row'>
+                        <div class='col-md-4 mx-auto mt-5 p-3 border'>
+                            <form action='' method='post'>
+                                <div class='form-row my-2'>
+                                    <input type='text' name='tableName' placeholder='Masa adı giriniz.' class='form-control' value='" . $table['name'] . "' />
+                                </div>
+                                <div class='form-row d-flex align-items-center justify-content-end'>
+                                    <input type='hidden' name='tableId' value='" . $tableId . "' />
+                                    <input type='submit' name='btnUpdate' value='Güncelle' class='btn btn-info' />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                ";
+            }
+        }
+    }
+    
+    public function deleteTables($database)
+    {
+        if (isset($_GET['id'])) {
+            $tableId = $_GET['id'];
+            if (!$this->mainQuery($database, 'select * from orders where tableId=' . $tableId)) {
+                $this->mainQuery($database, 'delete from tables where id=' . $tableId);
+            }
+            $this->redirectToLink(self::DASHBOARD_URL + "?page=tables");
+        }
+    }
+}
--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -10,96 +10,122 @@ if (isset($database)) {
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <base href="http://localhost/Restaurant-Cafe-Automation/admin/pages/dashboard.php">
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin Paneli</title>
     <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
+
 <body>
 
-<div class="container-fluid" style="height: 100vh">
-    <div class="row h-100">
-        <!--        menu-->
-        <div class="col-md-2 bg-light p-0 m-0 shadow">
-            <div>
-                <a class="btn btn-secondary btn-block rounded-0" href="http://localhost/Restaurant-Cafe-Automation/"
-                   target="_blank">Sisteme Git</a>
-            </div>
-            <div class="py-4 text-center border-bottom bg-info bgColorOrange">
-                Hoşgeldiniz! - <?= $system->getUserName($database) ?>
-            </div>
-            <div class="p-2">
-                <div class="mb-1 p-2 border-bottom text-primary font-weight-bold">Masa Yönetimi</div>
-                <div class="my-1 p-2 border-bottom text-primary font-weight-bold">Ürün Yönetimi</div>
-                <div class="my-1 p-2 border-bottom text-primary font-weight-bold">Kategori Yönetimi</div>
-                <div class="my-1 p-2 border-bottom text-primary font-weight-bold">Rapor Yönetimi</div>
-                <div class="my-1 p-2 border-bottom text-primary font-weight-bold">Şifre Değiştir</div>
-                <div class="my-1 p-2">
-                    <a href="dashboard.php?page=logout" id="btnLogOut" class="btn-danger btn font-weight-bold">Çıkış
-                        yap</a>
+    <div class="container-fluid" style="height: 100vh">
+        <div class="row h-100">
+            <!--        menu-->
+            <div class="col-md-2 bg-light p-0 m-0 shadow">
+                <div>
+                    <a class="btn btn-secondary btn-block rounded-0" href="http://localhost/Restaurant-Cafe-Automation/" target="_blank">Sisteme Git</a>
+                </div>
+                <div class="py-4 text-center border-bottom bg-info bgColorOrange">
+                    Hoşgeldiniz! - <?= $system->getUserName($database) ?>
+                </div>
+                <div class="p-2">
+                    <div class="mb-1 p-2 border-bottom text-primary font-weight-bold">
+                        <a href="dashboard.php?page=tables" class="text-primary text-decoration-none">Masa Yönetimi</a>
+                    </div>
+                    <div class="my-1 p-2 border-bottom text-primary font-weight-bold">
+                        <a href="dashboard.php?page=products" class="text-primary text-decoration-none">Ürün Yönetimi</a>
+                    </div>
+                    <div class="my-1 p-2 border-bottom text-primary font-weight-bold">
+                        <a href="dashboard.php?page=categories" class="text-primary text-decoration-none">Kategori Yönetimi</a>
+                    </div>
+
+                    <div class="my-1 p-2 border-bottom text-primary font-weight-bold">
+                        <a href="dashboard.php?page=reports" class="text-primary text-decoration-none">Rapor Yönetimi</a>
+                    </div>
+                    <div class="my-1 p-2 border-bottom text-primary font-weight-bold">
+                        <a href="dashboard.php?page=passwords" class="text-primary text-decoration-none">Şifre Değiştir</a>
+                    </div>
+                    <div class="my-1 p-2">
+                        <a href="dashboard.php?page=logout" id="btnLogOut" class="text-danger text-decoration-none font-weight-bold">Çıkış
+                            yap</a>
+                    </div>
+                </div>
+                <div>
+                    <table class=" table-striped table-bordered w-100">
+                        <thead class="table-warning">
+                            <tr>
+                                <th class="p-2 text-center" colspan="2">Anlık Durum</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="p-2 border-right">Toplam Sipariş</td>
+                                <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, "orders") ?></td>
+                            </tr>
+                            <tr>
+                                <td class="p-2 border-right">Doluluk Oranı</td>
+                                <td class="p-2 font-weight-bold text-center"><?= $statistics->getSolidityRatio($database) ?></td>
+                            </tr>
+                            <tr>
+                                <td class="p-2 border-right">Toplam Masa</td>
+                                <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, "tables") ?></td>
+                            </tr>
+                            <tr>
+                                <td class="p-2 border-right">Toplam Kategori</td>
+                                <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, "categories") ?></td>
+                            </tr>
+                            <tr>
+                                <td class="p-2 border-right">Toplam Ürün</td>
+                                <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, 'products') ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="text-center mt-2">
+                        <span class="p-2 border-right">Tarih</span>
+                        <span class="p-2 font-weight-bold text-center"><?= date("d.m.Y") ?></span>
+                    </div>
                 </div>
             </div>
-            <div>
-                <table class=" table-striped table-bordered w-100">
-                    <thead class="table-warning">
-                    <tr>
-                        <th class="p-2 text-center" colspan="2">Anlık Durum</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td class="p-2 border-right">Toplam Sipariş</td>
-                        <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, "orders") ?></td>
-                    </tr>
-                    <tr>
-                        <td class="p-2 border-right">Doluluk Oranı</td>
-                        <td class="p-2 font-weight-bold text-center"><?= $statistics->getSolidityRatio($database) ?></td>
-                    </tr>
-                    <tr>
-                        <td class="p-2 border-right">Toplam Masa</td>
-                        <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, "tables") ?></td>
-                    </tr>
-                    <tr>
-                        <td class="p-2 border-right">Toplam Kategori</td>
-                        <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, "categories") ?></td>
-                    </tr>
-                    <tr>
-                        <td class="p-2 border-right">Toplam Ürün</td>
-                        <td class="p-2 font-weight-bold text-center"><?= $statistics->getDBTableCount($database, 'products') ?></td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div class="text-center mt-2">
-                    <span class="p-2 border-right">Tarih</span>
-                    <span class="p-2 font-weight-bold text-center"><?= date("d.m.Y") ?></span>
-                </div>
+            <!--        content area-->
+            <div class="col-md-10 p-3">
+                <?php
+                switch (@$_GET['page']) {
+                        /* Logout */
+                    case 'logout':
+                        $system->logOut();
+                        break;
+                        /* Tables */
+                    case 'tables':
+                        $system->tables($database);
+                        break;
+                    case 'add-tables':
+                        $system->addTables($database);
+                        break;
+                    case 'update-tables':
+                        $system->updateTables($database);
+                        break;
+                    case 'delete-tables':
+                        $system->deleteTables($database);
+                        break;                       
+                    default:
+                        echo 'Welcome to dashboard!';
+                        break;
+                }
+                ?>
             </div>
-        </div>
-        <!--        content area-->
-        <div class="col-md-10 p-3">
-            <?php
-            switch (@$_GET['page']) {
-                case 'logout':
-                    $system->logOut();
-                    break;
-                default:
-                    echo 'Welcome to dashboard!';
-                    break;
-            }
-            ?>
         </div>
     </div>
-</div>
 
-<!--    scripts tags-->
-<script src="../../assets/js/jquery.slim.min.js"></script>
-<script src="../../assets/js/popper.min.js"></script>
-<script src="../../assets/js/bootstrap.bundle.min.js"></script>
-<!--my js-->
+    <!--    scripts tags-->
+    <script src="../../assets/js/jquery.slim.min.js"></script>
+    <script src="../../assets/js/popper.min.js"></script>
+    <script src="../../assets/js/bootstrap.bundle.min.js"></script>
+    <!--my js-->
 </body>
+
 </html>
